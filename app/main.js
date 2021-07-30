76ef5d7ebd53c2f9f29e805de6b002a657a300b8
Emmanuel Ndukwe <chuksel95@gmail.com>
2021-07-30 05:43:10 +0100

    js commit

--- app/main.js
+++ app/main.js
@@ -1,23 +1,19 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-    getVisitCount();
-});
+
 
 
 const localApi = 'http://localhost:7071/api/VisitorCount';
 const functionApi = 'https://resumepage.azurewebsites.net/api/VisitorCount?code=I5VuMTUExKek0wEa7gJ5IS9eBybqYnxDpiPHJhnTWPaU3TbLiQPUdQ=='; 
 
-const getVisitCount = () => {
-    let count = 30;
+
     fetch(functionApi)
     .then(response => {
         return response.json()
     })
     .then(response => {
         console.log("Website called function API.");
-        count = response.count;
-        document.getElementById('counter').innerText = count;
+        Count = response.count;
+        document.getElementById('Counter').innerText = 'You are vistor number' + Count ;
     }).catch(function(error) {
         console.log(error);
       });
-    return count;
-}
+   
