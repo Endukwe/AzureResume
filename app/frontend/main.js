275692622ea8d34cd6c8733144ad3bce79a1606a
Endukwe <chuksel95@gmail.com>
2021-08-23 12:40:17 +0100

    FE update

--- app/frontend/main.js
+++ app/frontend/main.js
@@ -1,18 +1,23 @@
-
+window.addEventListener('DOMContentLoaded', (event) => {
+    getVisitCount();
 
 
 const localApi = 'http://localhost:7071/api/countfn';
 const functionApi = 'https://nresumepage.azurewebsites.net/api/countfn'
 
-    fetch(functionApi)
-    .then(response => {
-        return response.json()
-    })
-    .then(response => {
-        console.log("Website called function API.");
-        Count = response.count;
-        document.getElementById('Counter').innerText = 'You are vistor number' + Count ;
-    }).catch(function(error) {
-        console.log(error);
-      });
+    const getVisitCount = () => {
+        let count = 30;
+        fetch(functionApi)
+        .then(response => {
+            return response.json()
+        })
+        .then(response => {
+            console.log("Website called function API.");
+            count = response.count;
+            document.getElementById('counter').innerText = count;
+        }).catch(function(error) {
+            console.log(error);
+          });
+        return count;
+    }
    
