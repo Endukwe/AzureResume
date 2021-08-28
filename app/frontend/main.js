7317a2ebbae3ddd3bf08e3d32399257cff2ec8b8
Endukwe <chuksel95@gmail.com>
2021-08-28 04:50:18 +0100

    main.js

--- app/frontend/main.js
+++ app/frontend/main.js
@@ -6,7 +6,7 @@ const localApi = 'http://localhost:7071/api/countfn';
 const functionApi = 'https://nresumepage.azurewebsites.net/api/countfn'
 
     const getVisitCount = () => {
-        
+        let visitorcount = 1;
         fetch(functionApi)
         .then(response => {
             return response.json()
